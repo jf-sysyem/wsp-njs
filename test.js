16107a4f25f4460a07d5da7f98b40fd6cfc62fe7
Ephraim <ephraim.pepe@gmail.com>
2013-11-04 23:47:11 +0100

    piccola modifica al test

--- test.js
+++ test.js
@@ -21,49 +21,32 @@ fs.readFile('./' + config + '.yml', 'utf8', function(err, data) {
     if (err) {
         return console.log(err);
     }
-    // Abilito il listener della chat
     test_port = ephp.readParam(data, 'port:', test_port);
     server.listen(test_port);
     console.log('Test service enabled on port ' + test_port);
 });
 
-// routing di express ??
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/example.html');
 });
 
-// username degli utenti che sono in chat
 var enter = exit = n = 0;
 
 setInterval(function(){
-    if(n > 1024*1024) n = 0;
+    if(n > 1024) n = 0;
     io.sockets.emit('counter', enter, exit, n++);
 }, 10000);
 
 io.sockets.on('connection', function(socket) {
 
-    /**
-     * emits 'adduser'
-     * 
-     * Aggiunge un utente in chat e lo fa entrare nella stanza default
-     * @var username string username adottato in chat
-     */
     socket.on('enter', function(user) {
-        // memorizzo l'username nella sessione del socket associata al client
         socket.user = user;
         enter++;
         socket.emit('private_msg', 'Ciao ' + socket.user);
         io.sockets.emit('public_msg', socket.user+' è entrato', socket.user);
     });
     
-    /**
-     * emits 'adduser'
-     * 
-     * Aggiunge un utente in chat e lo fa entrare nella stanza default
-     * @var username string username adottato in chat
-     */
     socket.on('exit', function(user) {
-        // memorizzo l'username nella sessione del socket associata al client
         exit++;
         socket.emit('private_msg', 'Arrivederci ' + socket.user);
         io.sockets.emit('public_msg', socket.user+' è uscito', socket.user);
